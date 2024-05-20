f2f91e70ac4182ced4e3861bb7db6b4828677c96
TemhaN <temhan2005s@gmail.com>
2024-05-20 08:13:36 +0500

    COPYRIGHT

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,5 @@
-import axios from "axios"
+import axios from 'axios';
 
 export const api = axios.create({
-	baseURL: 'http://127.0.0.1:8000/api/v1',
+	baseURL: 'http://192.168.205.86:80/api/v1',
 });
